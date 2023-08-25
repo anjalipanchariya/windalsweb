68fd1edd31075465e31cca8ce96c694e00e6d2d2
ankush <ankushkaudi147@gmail.com>
2023-08-25 14:31:37 +0530

    commit

--- client/src/components/product/addProduct.js
+++ client/src/components/product/addProduct.js
@@ -1,99 +1,130 @@
-import React, { useState } from 'react';
-import { Table, Button } from 'react-bootstrap';
-import './addProduct.css';
-import toast, { Toaster } from 'react-hot-toast';
-import { addProduct } from '../helper/helper';
+import React, { useState } from "react";
+import { Button, Form } from 'react-bootstrap';
+import './addProduct.css'
 
 function AddProduct() {
-  const [parameters, setParameters] = useState([]);
-  const [productName, setProductName] = useState('');
-
-  const addRow = () => {
-    const newParameter = { name: '', min: '', max: '', unit: '' };
-    setParameters([...parameters, newParameter]);
-  };
-
-  const handleParameterChange = (index, field, value) => {
-    const updatedParameters = [...parameters];
-    updatedParameters[index][field] = value;
-    setParameters(updatedParameters);
-  };
-
-  const handleSave = () => {
-    // You can now send the productName and parameters array to the backend for saving.
-    const addProductProsie = addProduct(productName,parameters)
-    addProductProsie.then((result)=>{
-      toast.success(result.msg)
-      setParameters([])
-      setProductName([])
-    }).catch((err)=>{
-      toast.error(err.msg)
+    const [formData, setFormData] = useState({
+        productName: '',
+        maximum: '',
+        minimum: '',
+        unit: ''
     })
-  };
 
-  return (
-    <div>
-      <Toaster position="top-center" reverseOrder={false}></Toaster>
-      <div className="productadd">
-        <h3>Product name:</h3>
-        <input
-          type="text"
-          value={productName}
-          placeholder="Enter Product Name"
-          onChange={(e) => setProductName(e.target.value)}
-        />
-        <Button onClick={addRow}>Add parameter</Button>
-        <Button onClick={handleSave}>Save</Button>
-      </div>
+    const [errors, setErrors] = useState({
+        productName: '',
+        maximum: '',
+        minimum: '',
+        unit: ''
+    })
 
-      <Table striped responsive hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Max</th>
-            <th>Min</th>
-            <th>Unit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {parameters.map((parameter, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>
-                <input
-                  type="text"
-                  value={parameter.name}
-                  onChange={(e) => handleParameterChange(index, 'name', e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  value={parameter.max}
-                  onChange={(e) => handleParameterChange(index, 'max', e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  value={parameter.min}
-                  onChange={(e) => handleParameterChange(index, 'min', e.target.value)}
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={parameter.unit}
-                  onChange={(e) => handleParameterChange(index, 'unit', e.target.value)}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
-  );
+    const validateProductName = () => {
+        if (formData.productName.trim() === '') {
+            setErrors((prevErrors) => ({ ...prevErrors, productName: 'Product Name is required' }));
+            return false;
+        }
+        return true;
+    };
+
+    const validateMaximum = () => {
+        if (!formData.maximum || parseFloat(formData.maximum) === 0) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                maximum: 'Must be a non-zero number',
+            }));
+            return false;
+        }
+        return true;
+    }
+
+    const validateMinimum = () => {
+        if (!formData.minimum || parseFloat(formData.minimum) === 0) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                minimum: 'Must be a non-zero number',
+            }));
+            return false;
+        }
+        return true;
+    }
+
+    const validateUnit = () => {
+        if (formData.unit.trim() === '') {
+            setErrors((prevErrors) => ({ ...prevErrors, unit: 'Unit is required' }));
+            return false;
+        }
+        return true;
+    };
+
+    const validateForm = () => {
+        const isValidProductName = validateProductName();
+        const isValidMaximum = validateMaximum();
+        const isValidMinimum = validateMinimum();
+        const isValidUnit = validateUnit();
+        return isValidProductName && isValidMaximum && isValidMinimum && isValidUnit;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (validateForm()) {
+            console.log("Form Submitted : ", formData);
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            [name]: '',
+        }));
+    }
+
+    return (
+        <div>
+            <div className="header-add-product">
+                <h2 className="add-product-header">Add Product</h2>
+            </div>
+            <div className="add-product-container">
+                <div className="add-product-inputs">
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control type="text" placeholder="Enter Product Name" name='productName' value={formData.productName} onChange={handleChange} />
+                            {errors.productName && <Form.Text className="text-danger">{errors.productName}</Form.Text>}
+                        </Form.Group>
+
+                        <Form.Select className="mb-3 select-param" aria-label="Default select example">
+                            <option>Select Parameters</option>
+                            <option value="1">Parameter 1</option>
+                            <option value="2">Parameter 2</option>
+                            <option value="3">Parameter 3</option>
+                        </Form.Select>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control type="number" placeholder="Maximum" name="maximum" value={formData.maximum} onChange={handleChange} />
+                            {errors.maximum && <Form.Text className="text-danger">{errors.maximum}</Form.Text>}
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control type="number" placeholder="Minimum" name="minimum" value={formData.minimum} onChange={handleChange} />
+                            {errors.minimum && <Form.Text className="text-danger">{errors.minimum}</Form.Text>}
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Control type="text" placeholder="Unit" name="unit" value={formData.unit} onChange={handleChange} />
+                            {errors.unit && <Form.Text className="text-danger">{errors.unit}</Form.Text>}
+                        </Form.Group>
+
+                        <Button variant="danger" className="add-button" type="submit">Add Product</Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+
 
 export default AddProduct;
